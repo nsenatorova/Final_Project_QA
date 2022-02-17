656fe04e9e388d080e2c618429e052bd60444f9b
Natalia Senatorova <n-senat@mail.ru>
2022-02-17 20:59:02 +0300

    fix transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -7,12 +7,16 @@ link = "http://selenium1py.pythonanywhere.com/"
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+    # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
